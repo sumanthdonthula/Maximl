0b1f5eead4f18dbf10d0bbb22cc41d04bec3b01c
sumanthdonthula <46747610+sumanthdonthula@users.noreply.github.com>
2020-11-21 20:06:37 +0530

    Create maximl.py

---
+++ maximl.py
@@ -0,0 +1,46 @@
+
+NO_OF_CHARS = 256
+
+
+def max_distinct_char(str, n): 
+
+	
+	count = [0] * NO_OF_CHARS 
+	
+	for i in range(n): 
+		count[ord(str[i])] += 1
+	
+	max_distinct = 0
+	for i in range(NO_OF_CHARS): 
+		if (count[i] != 0): 
+			max_distinct += 1	
+	
+	return max_distinct 
+
+def smallesteSubstr_maxDistictChar(str): 
+
+	n = len(str)	 
+	max_distinct = max_distinct_char(str, n) 
+	minl = n	 
+	for i in range(n): 
+		for j in range(n): 
+			subs = str[i:j] 
+			subs_length = len(subs) 
+			sub_distinct_char = max_distinct_char(subs,subs_length) 
+			
+			
+			if (subs_length < minl and
+				max_distinct == sub_distinct_char): 
+				minl = subs_length
+
+	return minl 
+
+ 
+if __name__ == "__main__": 
+	
+	
+	str = input()
+	
+	l = smallesteSubstr_maxDistictChar(str); 
+	print(l) 
+
